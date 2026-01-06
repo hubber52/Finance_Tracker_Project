ec88906da3fd4ceb0221ff6471ebd696fe1969bf
hubber52 <tommy.liang200@gmail.com>
2026-01-06 02:09:08 +0000

    validtion

--- Finance_Tracker/messages/spec/rails_helper.rb
+++ Finance_Tracker/messages/spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 
 ENV['RAILS_ENV'] = 'test'
+#File.expand_path('../config/environment', __dir__)
 require_relative '../config/environment'
 
 require 'spec_helper'
